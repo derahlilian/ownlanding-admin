8faf2f62a1d2219cd246d4227fccb3a89ca0f5c8
Richard <rcezea007@gmail.com>
2024-11-25 21:36:12 +0100

    updated database query logic

--- libs/Administration.php
+++ libs/Administration.php
@@ -34,18 +34,41 @@ class Administration {
         return $stmt->fetch($this->db::FETCH_ASSOC);
     }
 
-    function updateUser(int $user_id, string $name, string $last_name, string $email): bool
+    function updateUser(int $user_id, array $fields): bool
     {
-        $sql = "UPDATE `users` SET `name` = ?, `last_name` = ?, `email` = ? WHERE `id` = ?";
-        $stmt = $this->prepare($sql);
-        $stmt->execute([$name, $last_name, $email, $user_id]);
+        $updated_at = date("Y-m-d H:i:s");
+        $statement = [];
+        $values = [];
+
+        foreach ($fields as $field => $value) {
+            if ($value !== null) {
+                $statement[] = "`$field` = ?";
+                $values[] = $value;
+            }
+        }
+
+        $statement[] = "`updated_at` = ?";
+        $values[] = $updated_at;
+
+        if (empty($statement)) {
+            return false;
+        }
+
+        error_log(var_export($statement, true));
+        exit();
+
+
+        $sql = "UPDATE `users` SET " . implode(", ", $statement) . " WHERE `id` = ?";
+        $values[] = $user_id;
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute($values);
         return $stmt->rowCount() === 1;
     }
 
     function deactivateUser(int $user_id): bool
     {
         $sql = "UPDATE `users` SET `active` = 0 WHERE `id` = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$user_id]);
         return $stmt->rowCount() === 1;
     }
@@ -53,7 +76,7 @@ class Administration {
     function activateUserById(int $user_id): bool
     {
         $sql = "UPDATE `users` SET `active` = 1 WHERE `id` = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$user_id]);
         return $stmt->rowCount() === 1;
     }
@@ -61,7 +84,7 @@ class Administration {
     function deleteUserById(int $user_id): bool
     {
         $sql = "DELETE FROM `users` WHERE `id` = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$user_id]);
         return $stmt->rowCount() === 1;
     }
@@ -70,6 +93,13 @@ class Administration {
     /*
      * Fetch All Packages
      */
+    function getAllPackages(): false|array
+    {
+        $sql = "SELECT packages.id, package_code, packages_size, package_amount, estate_name, color_code, packages.created_at, location_name FROM packages JOIN locations on packages.location_id = locations.id ORDER BY created_at DESC";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
+    }
     function getPackages($locationId): false|array
     {
         $sql = "SELECT packages.id, package_code, packages_size, package_amount, estate_name, color_code, packages.created_at, location_name, (select count(id) from subscriptions where package_id = packages.id) as subscr, (select count(id) from subscriptions where package_id = packages.id and status = 'active') as paid  FROM packages  left JOIN locations on packages.location_id = locations.id WHERE location_id = ? ORDER BY created_at DESC";
@@ -81,9 +111,9 @@ class Administration {
     function getPackageByCode(string $package_code): false|array
     {
         $sql = "SELECT package_code FROM `packages` WHERE `package_code` = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$package_code]);
-        return $stmt->fetch(self::FETCH_ASSOC);
+        return $stmt->fetch($this->db::FETCH_ASSOC);
     }
 
     /*
@@ -92,7 +122,7 @@ class Administration {
     public function createNewPackage(array $data): bool
     {
         $sql = "INSERT INTO packages (package_code, packages_size, package_amount, estate_name, color_code, location_id) VALUES (?, ?, ?, ?, ?, ?)";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$data["package_code"], $data["packages_size"], $data["package_amount"], $data["estate_name"], $data["color_code"], $data["location_id"]]);
         return $stmt->rowCount() === 1;
     }
@@ -114,7 +144,7 @@ class Administration {
     function createNewState($state_name, $county_id): bool
     {
         $sql = "INSERT INTO `states` (`state_name`, `country_id`) VALUES (?, ?)";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$state_name, $county_id]);
         return $stmt->rowCount() === 1;
     }
@@ -125,7 +155,7 @@ class Administration {
     function createNewLocation($location_name, $state_id): bool
     {
         $sql = "INSERT INTO `locations` (`location_name`, `state_id`) VALUES (?, ?)";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$location_name, $state_id]);
         return $stmt->rowCount() === 1;
     }
@@ -160,25 +190,25 @@ class Administration {
     function getCountryByName(string $country_name): false|array
     {
         $sql = "SELECT id, country_name FROM countries WHERE country_name = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$country_name]);
-        return $stmt->fetch(self::FETCH_ASSOC);
+        return $stmt->fetch($this->db::FETCH_ASSOC);
     }
 
     function getStateByNameAndCountryId(string $state_name, string $country_id): false|array
     {
         $sql = "SELECT id, state_name FROM states WHERE country_id = ? AND state_name = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$country_id, $state_name]);
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
     function getLocationByNameAndStateId(string $location_name, string $state_id): false|array
     {
         $sql = "SELECT id, location_name FROM locations WHERE location_name = ? AND state_id = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$location_name, $state_id]);
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
     /*
@@ -194,6 +224,17 @@ class Administration {
 
     /*
      * Fetch All Locations
+     */
+    public function getAllLocations(): array|false
+    {
+        $sql = "SELECT id, location_name FROM locations";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
+    }
+
+    /*
+     * Fetch All Locations by state id
      */
     public function getLocations($id): array|false
     {
@@ -220,9 +261,9 @@ class Administration {
         LEFT JOIN payments ON payment_reference = payments.id
         LEFT JOIN packages ON package_id = packages.id
         ORDER BY payments.created_at DESC";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute();
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
     function getSubscriptionsByPackageId(int $package_id): array
@@ -242,10 +283,12 @@ class Administration {
     function getAllPayments(): false|array
     {
         $sql = "SELECT channel, payments.created_at, reference, amount, status, name, last_name FROM payments JOIN users ON payments.user_id = users.id ORDER BY payments.created_at DESC";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute();
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
+
+
 
     function getSGA(): false|array
     {
@@ -257,23 +300,25 @@ class Administration {
         return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
-    function getAllTransactions(): false|array
+    function getAllTransactions($payout = false): false|array
     {
+        !$payout ? $line = "WHERE transactions.type = ? AND transactions.status != ?" : $line = "WHERE transactions.type != ? AND transactions.status = ?";
         $sql = "SELECT transactions.created_at, transactions.description, transactions.transaction_amount, payments.reference, transactions.type, transactions.status, users.name, users.last_name FROM transactions
                 LEFT JOIN users ON transactions.user_id = users.id
                 LEFT JOIN payments ON transactions.payment_id = payments.id
+                $line
                 ORDER BY transactions.created_at DESC";
-        $stmt = $this->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(['credit', 'pending']);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
     function getUserTransactions(int $user_id): false|array
     {
         $sql = "SELECT * FROM transactions LEFT JOIN payments ON transactions.payment_id = payments.id WHERE transactions.user_id = ?";
-        $stmt = $this->prepare($sql);
+        $stmt = $this->db->prepare($sql);
         $stmt->execute([$user_id]);
-        return $stmt->fetchAll(self::FETCH_ASSOC);
+        return $stmt->fetchAll($this->db::FETCH_ASSOC);
     }
 
 }
