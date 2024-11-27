777fcdd8665c8754abb5c8ca3ed737dc488af20d
Derah <derahlilian2000@gmail.com>
2024-11-27 11:56:29 +0100

    packages-and-property

--- page_contents/package.php
+++ page_contents/package.php
@@ -72,13 +72,9 @@
 
                                                     <div class="mb-3">
                                                         <label class="text-label form-label" for="dlab-password">Color Code*</label>
-                                                        <div class="input-group transparent-append">
-                                                        <div class="example">
-                                                            <input type="text" name="color_code" class="complex-colorpicker form-control" value="#364e29">
-                                                        </div>
-                                                        </div>
+                                                            <input type="color" name="color_code" class="complex-colorpicker form-control" value="#364e29">
+                                                        
                                                     </div>
-
                                                     <div class="mb-3">
                                                         <div class="form-check">
                                                             <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
@@ -140,10 +136,7 @@
 												</td>
 												<td>
 													<div class="concierge-bx d-flex align-items-center" style="cursor: pointer; transition: background-color 0.3s ease-in-out;">
-														<!-- <a href="user-details.php">
-															<img class="me-3 rounded profile-img" src="public/assets/images/avatar/2.jpg" alt="Guest Avatar" 
-																style="transition: transform 0.6s ease-in-out, box-shadow 0.3s ease-in-out;" />
-														</a> -->
+														
 														<div>
 															<h5 class="fs-16 mb-0 text-nowrap">
 																<a class="text-black hover-text" href="javascript:void(0);" 
@@ -151,7 +144,6 @@
 																<?php echo $package['package_code']; ?>
 																</a>
 															</h5>
-															<!-- <span class="text-primary fs-14">#ABJ-00002</span> -->
 														</div>
 													</div>
 												</td>
@@ -177,21 +169,85 @@
 													<div class="request">
 														<a href="/ownlanding-admin/controllers/Administrator.php?packageId=<?php echo $package['id'];?>" class="btn btn-md text-secondary">View Subscription</a>
 													</div>
+													
 												</td>
 												<td>
-													<div class="dropdown dropend">
-														<a href="javascript:void(0);" class="btn-link" data-bs-toggle="dropdown" aria-expanded="false">
-															<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																<path d="M11 12C11 12.5523 11.4477 13 12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12Z" stroke="#262626" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-																<path d="M18 12C18 12.5523 18.4477 13 19 13C19.5523 13 20 12.5523 20 12C20 11.4477 19.5523 11 19 11C18.4477 11 18 11.4477 18 12Z" stroke="#262626" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-																<path d="M4 12C4 12.5523 4.44772 13 5 13C5.55228 13 6 12.5523 6 12C6 11.4477 5.55228 11 5 11C4.44772 11 4 11.4477 4 12Z" stroke="#262626" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-															</svg>
-														</a>
-														<div class="dropdown-menu">
-															<a class="dropdown-item" href="javascript:void(0);">Edit</a>
-															<a class="dropdown-item" href="javascript:void(0);">Delete</a>
-														</div>
-													</div>
+												<button type="button" class="btn btn-secondary mb-2" data-bs-toggle="modal" data-bs-target="#editPackage">Edit Package</button>
+																	<!-- Modal -->
+																	<div class="modal fade" id="editPackage">
+																		<div class="modal-dialog">
+																			<div class="modal-content">
+																				<div class="modal-header">
+																					<h5 class="modal-title">Edit Package Details</h5>
+																					<button type="button" class="btn-close" data-bs-dismiss="modal">
+																					</button>
+																				</div>
+																				<div class="modal-body">
+																					<div class="col-lg-12">
+																						<div class="card">
+																							<div class="card-header">
+																								<h4 class="card-title">Please Fill All Field!</h4>
+																							</div>
+																							<div class="card-body">
+																								<div class="basic-form">
+																									<form class="form-valide-with-icon needs-validation" method="post" action="/ownlanding-admin/controllers/Administrator.php">
+																										<div class="mb-3">
+																											<label class="text-label form-label" for="validationCustomUsername">Package Code</label>
+																											<div class="input-group">
+																												<span class="input-group-text"> <i class="fa fa-user"></i> </span>
+																												<input type="text" class="form-control" id="validationCustomUsername" name="package_code" placeholder="fetch previous package code" required>
+																											</div>
+																										</div>
+																										<div class="mb-3">
+																											<label class="text-label form-label" for="validationCustomUsername">Package Size</label>
+																											<div class="input-group">
+																												<span class="input-group-text"> <i class="fa fa-user"></i> </span>
+																												<input type="text" class="form-control" id="validationCustomUsername" name="package_size" placeholder="Get previous package size" required>
+																											</div>
+																										</div>
+																										<div class="mb-3">
+																											<label class="text-label form-label" for="validationCustomUsername">Package Amount</label>
+																											<div class="input-group">
+																												<span class="input-group-text"> <i class="fa fa-user"></i> </span>
+																												<input type="number" class="form-control" id="validationCustomUsername" name="package_amount" placeholder="fetch package price" required>
+																											</div>
+																										</div>
+																										<div class="mb-3">
+																											<label class="text-label form-label" for="validationCustomUsername">Estate Name</label>
+																											<div class="input-group">
+																												<span class="input-group-text"> <i class="fa fa-user"></i> </span>
+																												<input type="text" class="form-control" id="validationCustomUsername" name="estate_name" placeholder="fetch estate name" required>
+																											</div>
+																										</div>
+																										<div class="mb-3">
+																											<label class="text-label form-label" for="validationCustomUsername">Location</label>
+																											<div class="input-group">
+																											<button class="btn btn-secondary" type="button">Location</button>
+																											<select name="location_id" class="default-select form-control wide">
+																												<option value="" selected>Choose preferred</option>
+																												<option value="france">france</option>
+																											</select>
+																											</div>
+																										</div>
+																										<div class="mb-3">
+																											<div class="form-check">
+																												<input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+																												<label class="form-check-label" for="invalidCheck2">
+																												I Agree to Changes Made To This Package.
+																												</label>
+																											</div>
+																										</div>
+																										<button type="submit" name="createPackage" class="btn me-2 btn-secondary">Submit</button>
+																										<button class="btn btn-light" data-bs-dismiss="modal">Cancel</button>
+																									</form>
+																								</div>
+																							</div>
+																						</div>
+																					</div>
+																				</div>
+																			</div>
+																		</div>
+																	</div>
 												</td>
 											</tr>
                                         <?php endforeach; ?>
